d9ece11d2f252bee4cdabe4e00fe20e5d9b82a29
Jesus Gabriel Tolosa Z <ing.tolosa@gmail.com>
2023-01-17 10:35:01 -0500

    creacion proyecto microservicio usuario-servicio

---
+++ usuario-servicio/src/main/java/com/usuario/servicio/app/UsuarioServicioApplication.java
@@ -0,0 +1,13 @@
+package com.usuario.servicio.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioServicioApplication.class, args);
+	}
+
+}
