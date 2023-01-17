0129f8c7d88afe442c85f215b64086e42d0f53f6
Jesus Gabriel Tolosa Z <ing.tolosa@gmail.com>
2023-01-17 15:11:23 -0500

    apirest de moto

---
+++ moto-servicio/src/main/java/com/moto/service/app/repository/MotoRepository.java
@@ -0,0 +1,13 @@
+package com.moto.service.app.repository;
+
+import com.moto.service.app.entity.Moto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MotoRepository extends JpaRepository<Moto, Integer> {
+
+    List<Moto> findByUsuarioId(int usuarioId);
+}
